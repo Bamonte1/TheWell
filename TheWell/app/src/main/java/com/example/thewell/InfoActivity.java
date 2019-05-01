c781091381c197d84add12434f01107a9b083740
Jacob Bamonte <jab65@pct.edu>
2019-05-01 14:56:12 -0400

    Created info form

---
+++ TheWell/app/src/main/java/com/example/thewell/InfoActivity.java
@@ -0,0 +1,88 @@
+package com.example.thewell;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+public class InfoActivity extends AppCompatActivity {
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch(item.getItemId()) {
+            case R.id.action_phone:
+                showToast("(570) 246-5585");
+                return true;
+            case R.id.action_home:
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                finish();
+                startActivity(intent);
+                return true;
+            case R.id.action_mail:
+                showToast("kyle.anspach@cwc.life");
+                return true;
+            case R.id.action_map:
+                if(isServicesReady()) {
+                    init();
+                }
+                return true;
+            case R.id.action_sound:
+                showToast("sound");
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showToast (String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    private void init() {
+
+        Intent intent = new Intent(getApplicationContext(), ContactActivity.class);
+        finish();
+        startActivity(intent);
+
+    }
+
+    public boolean isServicesReady() {
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(InfoActivity.this);
+
+        if(available == ConnectionResult.SUCCESS) {
+
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(InfoActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this, "Sorry maps cannot be displayed", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+}
